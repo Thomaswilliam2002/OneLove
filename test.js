45b588c2d1afb23a9489c7531df630d36b8020c7
William Hounwanou <williamhounwanou2002@email.com>
2026-02-25 19:38:08 +0100

    mm jour 19:38

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
 console.log(process.env.PORT)
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST || 'localhost',
+        dialect: 'mysql', //'mariadb',
+        dialectModule: require('mysql2'), // Optionnel mais conseillé pour éviter des erreurs au déploiement
+        port: process.env.DB_PORT || 3306
+    }
+)
