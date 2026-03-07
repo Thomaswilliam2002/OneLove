505f01042e22172965881c12c3b6dbee5184bd82
William Hounwanou <williamhounwanou2002@email.com>
2026-03-07 22:36:36 +0100

    Commit corection nom de la base de donnee

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -37,28 +37,16 @@ const CategorieDepense_m = require('../models/categorieDepense');
 const CaissePersonnel_m = require('../models/caissePersonnel');
 const { on } = require('nodemailer/lib/xoauth2');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST || 'localhost',
-        dialect: 'mariadb',
-        // dialectModule: require('mysql2'), // Optionnel mais conseillé pour éviter des erreurs au déploiement
-        port: process.env.DB_PORT || 3306
-    }
-)
-
 // On utilise l'URL de Render (DATABASE_URL)
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect: 'postgres',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false // Indispensable pour que Render accepte la connexion
-//       }
-//     }
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Indispensable pour que Render accepte la connexion
+      }
+    }
+});
 
 const Appartement = Appartement_m(sequelize, DataTypes);
 const AppartJournal = AppartJournal_m(sequelize, DataTypes);
