6e2407c4aaa31046ed2d04e3095baa382730cc88
William Hounwanou <williamhounwanou2002@email.com>
2026-03-08 16:20:37 +0100

    commit

--- index.js
+++ index.js
@@ -178,46 +178,46 @@ app.get('/recap', protrctionRoot, authorise('admin','comptable'), async (req, re
             nb_mc, nb_ch, nb_cu, nb_cat, nb_prod, nb_emb, nb_cai, sum_depense
         ] = await Promise.all([
 
-            Personnel.count({ where: { is_active: true } }),
-            Appartement.count({ where: { is_active: true } }),
-            BarSimple.count({ where: { is_active: true } }),
-            BarVip.count({ where: { is_active: true } }),
-            CrazyClub.count({ where: { is_active: true } }),
+            Personnel.count(),
+            Appartement.count(),
+            BarSimple.count(),
+            BarVip.count(),
+            CrazyClub.count(),
 
             BarSimpleJournal.sum("recette", {
-                where: { is_active: true ,date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
+                where: { date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
             }),
 
             BarVipJournal.sum("recette", {
-                where: { is_active: true ,date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
+                where: { date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
             }),
 
             CrazyClubJournal.sum("recette", {
-                where: { is_active: true ,date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
+                where: { date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
             }),
 
             CuisineJournal.sum("montant_verser", {
-                where: { is_active: true ,date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
+                where: { date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
             }),
 
             AppartJournal.sum("loyer", {
-                where: { is_active: true ,date_debut: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
+                where: { date_debut: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
             }),
 
             ChambreJournal.sum("loyer", {
-                where: { is_active: true ,date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
+                where: { date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
             }),
 
-            MaisonColse.count({ where: { is_active: true } }),
-            Chambre.count({ where: { is_active: true } }),
-            Cuisine.count({ where: { is_active: true } }),
-            Categorie.count({ where: { is_active: true } }),
-            Produit.count({ where: { is_active: true } }),
-            Emballage.count({ where: { is_active: true } }),
-            Caisse.count({ where: { is_active: true } }),
+            MaisonColse.count(),
+            Chambre.count(),
+            Cuisine.count(),
+            Categorie.count(),
+            Produit.count(),
+            Emballage.count(),
+            Caisse.count(),
 
             Depense.sum("montant", {
-                where: { is_active: true ,date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
+                where: { date: { [Op.gte]: todayStart, [Op.lt]: todayEnd } }
             })
         ]);
 
@@ -264,31 +264,31 @@ app.get('/index', protrctionRoot, authorise('admin'), async (req, res) => {
         const [
             nb_personnel, nb_appart, nb_barSimple, nb_barVip, nb_crazyClub,
             sum_bs, sum_bv, sum_cc, sum_cui, sum_ap, sum_ch,
-            nb_mc, nb_ch, nb_cu, nb_cat, nb_prod, nb_emb, nb_cai,sum_depense
+            ecette, nb_mc, nb_ch, nb_cu, nb_cat, nb_prod, nb_emb, nb_cai,sum_depense
         ] = await Promise.all([
-            Personnel.count({ where: { is_active: true } }),
-            Appartement.count({ where: { is_active: true } }),
-            BarSimple.count({ where: { is_active: true } }),
-            BarVip.count({ where: { is_active: true } }),
-            CrazyClub.count({ where: { is_active: true } }),
+            Personnel.count(),
+            Appartement.count(),
+            BarSimple.count(),
+            BarVip.count(),
+            CrazyClub.count(),
             // Sommes avec gestion de la période (Gte = Supérieur ou égal, Lt = Strictement inférieur)
              // -----------------------------------------------------------
             
-            BarSimpleJournal.sum("recette", { where: { is_active: true ,date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
-            BarVipJournal.sum("recette", { where: { is_active: true ,date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
-            CrazyClubJournal.sum("recette", { where: { is_active: true ,date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
-            CuisineJournal.sum("montant_verser", { is_active: true ,where: { date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
-            AppartJournal.sum("loyer", { where: { is_active: true ,date_debut: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
-            ChambreJournal.sum("loyer", { where: { is_active: true ,date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
+            BarSimpleJournal.sum("recette", { where: { date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
+            BarVipJournal.sum("recette", { where: { date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
+            CrazyClubJournal.sum("recette", { where: { date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
+            CuisineJournal.sum("montant_verser", { where: { date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
+            AppartJournal.sum("loyer", { where: { date_debut: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
+            ChambreJournal.sum("loyer", { where: { date: { [Op.gte]: firstDay, [Op.lt]: lastDay } } }),
             // Totaux globaux
-            MaisonColse.count({ where: { is_active: true } }),
-            Chambre.count({ where: { is_active: true } }),
-            Cuisine.count({ where: { is_active: true } }),
-            Categorie.count({ where: { is_active: true } }),
-            Produit.count({ where: { is_active: true } }),
-            Emballage.count({ where: { is_active: true } }),
-            Caisse.count({ where: { is_active: true } }),
-            Depense.sum("montant", { is_active: true ,where: {date: {[Op.gte]: firstDay,[Op.lt]: lastDay } } })
+            MaisonColse.count(),
+            Chambre.count(),
+            Cuisine.count(),
+            Categorie.count(),
+            Produit.count(),
+            Emballage.count(),
+            Caisse.count(),
+            Depense.sum("montant", {where: {date: {[Op.gte]: firstDay,[Op.lt]: lastDay } } })
         ]);
 
         const data = {
@@ -328,9 +328,9 @@ stockJob.start();
 //route presence
 app.get('/presence', protrctionRoot, authorise('admin', 'comptable', 'caissier'), async (req, res) => {
     const presences = await Presence.findAll({
-        include:[{model: Personnel, required: false, where: { is_active: true }}],
+        include:[{model: Personnel}]
     })
-    const personnels = await Personnel.findAll({where: { is_active: true }});
+    const personnels = await Personnel.findAll();
     if(presences){
         res.render('presence', {presences: presences,personnels: personnels, msg: req.query.msg, tc: req.query.text_color});
     }else{
@@ -341,17 +341,16 @@ app.get('/presence', protrctionRoot, authorise('admin', 'comptable', 'caissier')
 
 //route login
 app.get('/login', (req, res) => {
-    res.render('login', {msg: req.query.msg, tc: req.query.tc})
+    res.render('login', {msg: req.query.msg})
 })
 
 //route connexion
 app.post('/connexion', async (req, res) => {
     const{email, pwd} = req.body
     const personnel = await Occupe.findOne({
-        where: {is_active: true},
         include:[
-            {model: Personnel,where: {email: email, is_active: true}, required: false},
-            {model: Poste, required: false, where: {is_active: true}}
+            {model: Personnel,where: {email: email}},
+            {model: Poste}
         ]
     })
     if(personnel){
@@ -370,16 +369,16 @@ app.post('/connexion', async (req, res) => {
                 }
             }else{
                 // console.log('compte nom valider');
-                res.redirect('login?msg= Votre compte n\'est pas valider. Veuillez pacienter ou contacter l\'administrateur.&tc=alert-danger')
+                res.redirect('login?msg=validation')
             }
             
         }else{
             // console.log('mdp incorrect');
-            res.redirect('login?msg=Email ou mot de passe incorrect&tc=alert-danger')
+            res.redirect('login?msg=mdp')
         }
     }else{
         // console.log('utilisateur non trouver');
-        res.redirect('login?msg=Utilisateur non trouver.Email ou mot de passe incorrect&tc=alert-danger')
+        res.redirect('login?msg=user')
     }
 })
 
@@ -442,14 +441,14 @@ app.get('/proforma', protrctionRoot, authorise('admin', 'comptable'), (req, res)
 
 //route add
 app.get('/Add', protrctionRoot, authorise('admin', 'comptable'), (req, res) => {
-    MaisonColse.findAll({where: {is_active: true}})
+    MaisonColse.findAll()
         .then(maisonColses => {
-            Chambre.findAll({where: {is_active: true}}) 
+            Chambre.findAll() 
                 .then(chambres => {
                     
                     const msg = "Liste recuperer avec succes"
                     //res.json(maisonColses[0].id_mclose)
-                    res.render('ajout', {maisonColses: maisonColses, chambres: chambres, msg: req.query.msg, tc: req.query.text_color})
+                    res.render('ajout', {maisonColses: maisonColses, chambres: chambres})
                 })
                 .catch(_ => console.log('erreure de selection all' + _))
         })
@@ -467,7 +466,7 @@ app.get('/editBarClub/:id', protrctionRoot, authorise('admin', 'comptable'), (re
     if(req.query.type === "bs"){
         BarSimple.findByPk(req.params.id)
             .then(bar => {
-                res.status(200).render('edit-bar-club', {bar: bar, type: "bs", })
+                res.status(200).render('edit-bar-club', {bar: bar, type: "bs"})
             })
             .catch(_ => console.log('erreure de selection' + _))
     }
@@ -491,13 +490,13 @@ app.get('/editBarClub/:id', protrctionRoot, authorise('admin', 'comptable'), (re
 
 //route afficher bar simple,vip and crazy club
 app.get('/allBarClub', protrctionRoot, authorise('admin', 'comptable'), (req, res) => {
-    BarSimple.findAll({where: {is_active: true}})
+    BarSimple.findAll()
         .then(bars => {
-            BarVip.findAll({where: {is_active: true}})
+            BarVip.findAll()
                 .then(barv => {
-                    CrazyClub.findAll({where: {is_active: true}})
+                    CrazyClub.findAll()
                         .then(crazyc => {
-                            res.render('all-bars-club', {bars: bars, barv: barv, crazyc: crazyc, msg: req.query.msg, type: req.query.type, tc: req.query.tc})
+                            res.render('all-bars-club', {bars: bars, barv: barv, crazyc: crazyc, msg: req.query.msg, type: req.query.type})
                         })
                         .catch(_ => console.log('erreure de selection all' + _))
                 })
@@ -511,16 +510,15 @@ app.get('/salarier', protrctionRoot, authorise('admin', 'comptable'), async (req
     const data = []
     try{
         const salariers = await Occupe.findAll({
-            where: {is_active: true},
             include:[
-                {model: Personnel, required: false, where: {is_active: true}},
-                {model: Poste, required: false, where: {is_active: true}}
+                {model: Personnel},
+                {model: Poste}
             ],
             order:[['id_occupe', 'DESC']]
         })
         if(salariers){
             data.push(salariers)
-            const sanction = await Sanction.findAll({where: {is_active: true}})
+            const sanction = await Sanction.findAll()
             if(sanction){
                 data.push(sanction)
                 res.render('salaries', {datas: data})
@@ -544,13 +542,13 @@ app.get('/salarier', protrctionRoot, authorise('admin', 'comptable'), async (req
 app.get('/formFondBarClub', protrctionRoot, authorise('admin','comptable','caissier', 'caissier central'), async (req, res) =>{
     let chambres = null;
     if(req.query.type === 'bc'){
-        BarSimple.findAll({where: {is_active: true}})
+        BarSimple.findAll()
         .then(barss =>{
-            BarVip.findAll({where: {is_active: true}})
+            BarVip.findAll()
                 .then(barvs =>{
-                    CrazyClub.findAll({where: {is_active: true}})
+                    CrazyClub.findAll()
                         .then(crazys =>{
-                            Caisse.findAll({where: {is_active: true}})
+                            Caisse.findAll()
                                 .then(caisses => {
                                     res.render('fondBarClub', {barss: barss, chambres: chambres, barvs: barvs, crazys: crazys, caisses: caisses, msg: req.query.msg, type: req.query.type})
                                 })
@@ -562,21 +560,21 @@ app.get('/formFondBarClub', protrctionRoot, authorise('admin','comptable','caiss
         })
         .catch(err => console.log(err))
     }else if(req.query.type === 'cuisine'){
-        Cuisine.findAll({where: {is_active: true}})
+        Cuisine.findAll()
             .then(cuisines => {
                 res.render('fondBarClub', {cuisines: cuisines, chambres: chambres, msg: req.query.msg, type: req.query.type})
             })
             .catch(_ => console.log('erreure de selection all' + _))
     }else if(req.query.type === 'mclose') {
-        const mcloses = await MaisonColse.findAll({where: {is_active: true}})
+        const mcloses = await MaisonColse.findAll()
         if(mcloses){
-            const chambre = await Chambre.findAll({where: {is_active: true}})
+            const chambre = await Chambre.findAll()
             if(chambre){
                 res.render('fondBarClub', {mcloses: mcloses, chambres: chambre, msg: req.query.msg, type: req.query.type})
             }
         }
     }else if(req.query.type === 'appart') {
-        const apparts = await Appartement.findAll({where: {is_active: true}})
+        const apparts = await Appartement.findAll()
         if(apparts && apparts !== null){
             res.render('fondBarClub', {apparts:apparts,chambres: chambres, msg: req.query.msg, type: req.query.type})
         }
@@ -595,43 +593,37 @@ app.get('/caisseOnelove', protrctionRoot, authorise('admin', 'comptable'), async
                 attributes: [[moisExpr, 'mois'], [fn('SUM', col('recette')), 'total_recette']],
                 group: [moisExpr], // Groupement par l'expression de date
                 order: [[literal('"mois"'), 'ASC']],
-                raw: true,
-                where: { is_active: true }
+                raw: true
             }),
             BarVipJournal.findAll({
                 attributes: [[moisExpr, 'mois'], [fn('SUM', col('recette')), 'total_recette']],
                 group: [moisExpr],
                 order: [[literal('"mois"'), 'ASC']],
-                raw: true,
-                where: { is_active: true }
+                raw: true
             }),
             AppartFondJournal.findAll({
                 attributes: [[moisExpr, 'mois'], [fn('SUM', col('recette')), 'total_recette']],
                 group: [moisExpr],
                 order: [[literal('"mois"'), 'ASC']],
-                raw: true,
-                where: { is_active: true }
+                raw: true
             }),
             CuisineJournal.findAll({
                 attributes: [[moisExpr, 'mois'], [fn('SUM', col('montant_verser')), 'total_recette']],
                 group: [moisExpr],
                 order: [[literal('"mois"'), 'ASC']],
-                raw: true,
-                where: { is_active: true }
+                raw: true
             }),
             ChambreJournal.findAll({
                 attributes: [[moisExpr, 'mois'], [fn('SUM', col('loyer')), 'total_recette']],
                 group: [moisExpr],
                 order: [[literal('"mois"'), 'ASC']],
-                raw: true,
-                where: { is_active: true }
+                raw: true
             }),
             CrazyClubJournal.findAll({ // Assure-toi que le modèle est bien importé
                 attributes: [[moisExpr, 'mois'], [fn('SUM', col('recette')), 'total_recette']],
                 group: [moisExpr],
                 order: [[literal('"mois"'), 'ASC']],
-                raw: true,
-                where: { is_active: true }
+                raw: true
             })
         ]);
 
